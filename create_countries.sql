81d48eddaa325e553cf7e6b7f597df248db2a0ca
reggae-krk <monika.gorecka.agh@gmail.com>
2018-10-03 10:05:16 +0200

    Add create_countries sql file

---
+++ create_countries.sql
@@ -0,0 +1,57 @@
+INSERT INTO countries (country_code, country_name, belongs_to_ue) VALUES ('POL', 'Poland', TRUE);
+
+INSERT INTO countries (country_code, country_name, belongs_to_ue) VALUES ('ARG', 'Argentina', FALSE);
+
+INSERT INTO countries (country_code, country_name, belongs_to_ue) VALUES ('AUS', 'Australia', FALSE);
+
+INSERT INTO countries (country_code, country_name, belongs_to_ue) VALUES ('AUT', 'Austria', TRUE);
+
+INSERT INTO countries (country_code, country_name, belongs_to_ue) VALUES ('BEL', 'Belgium', TRUE);
+
+INSERT INTO countries (country_code, country_name, belongs_to_ue) VALUES ('BLR', 'Belarus', FALSE);
+
+INSERT INTO countries (country_code, country_name, belongs_to_ue) VALUES ('BRA', 'Brazil', FALSE);
+
+INSERT INTO countries (country_code, country_name, belongs_to_ue) VALUES ('HRV', 'Croatia', TRUE);
+
+INSERT INTO countries (country_code, country_name, belongs_to_ue) VALUES ('CPR', 'Cyprus', TRUE);
+
+INSERT INTO countries (country_code, country_name, belongs_to_ue) VALUES ('CZE', 'Czechia', TRUE);
+
+INSERT INTO countries (country_code, country_name, belongs_to_ue) VALUES ('DNK', 'Denmark', TRUE);
+
+INSERT INTO countries (country_code, country_name, belongs_to_ue) VALUES ('FRA', 'France', TRUE);
+
+INSERT INTO countries (country_code, country_name, belongs_to_ue) VALUES ('ESP', 'Spain', TRUE);
+
+INSERT INTO countries (country_code, country_name, belongs_to_ue) VALUES ('NLD', 'Netherlands', TRUE);
+
+INSERT INTO countries (country_code, country_name, belongs_to_ue) VALUES ('CAN', 'Canada', FALSE);
+
+INSERT INTO countries (country_code, country_name, belongs_to_ue) VALUES ('QAT', 'Qatar', FALSE);
+
+INSERT INTO countries (country_code, country_name, belongs_to_ue) VALUES ('NOR', 'Norway', FALSE);
+
+INSERT INTO countries (country_code, country_name, belongs_to_ue) VALUES ('RUS', 'Russian Federation', FALSE);
+
+INSERT INTO countries (country_code, country_name, belongs_to_ue) VALUES ('ITA', 'Italy', TRUE);
+
+INSERT INTO countries (country_code, country_name, belongs_to_ue) VALUES ('GBR', 'United Kingdom', TRUE);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
