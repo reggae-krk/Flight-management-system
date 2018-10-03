c25171b5bbc2c7fa6adee273ec049abc490ff707
reggae-krk <monika.gorecka.agh@gmail.com>
2018-10-04 00:14:29 +0200

    Create trigger for time flight

--- triggers.sql
+++ triggers.sql
@@ -40,8 +40,61 @@ CREATE TRIGGER check_base_price
   FOR EACH ROW
   EXECUTE PROCEDURE check_if_base_price_is_bigger_than_0();
 
--- check trigger
--- INSERT INTO flights(SELECT generate_flight_id(), 22, to_timestamp('2018-10-01 10:00:00', 'YYYY-MM-DD HH24:MI:SS'), to_timestamp('2018-10-01 21:30:00', 'YYYY-MM-DD HH24:MI:SS'), null, null, 0::decimal);
+CREATE OR REPLACE FUNCTION check_if_time_flight_is_correct_update()
+  RETURNS trigger AS $$
+  DECLARE
+    minutes INTEGER;
+    interval_time VARCHAR(10);
+  BEGIN
+    IF OLD.flight_connection_id != NEW.flight_connection_id THEN
+      minutes = calculate_flight_duration((SELECT distance
+                                           FROM flight_connections
+                                           WHERE id = NEW.flight_connection_id));
+    ELSE
+      minutes = calculate_flight_duration((SELECT distance
+                                           FROM flight_connections
+                                           WHERE id = OLD.flight_connection_id));
+    END IF;
 
+    interval_time = minutes || ' min';
 
+    IF (NEW.scheduled_departure_time + interval_time::interval) != NEW.scheduled_arrival_time THEN
+      RAISE WARNING 'Flight time is wrong';
+      RETURN NULL;
+    end if;
 
+  RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER check_flight_time_update
+  BEFORE UPDATE ON flights
+  FOR EACH ROW
+  EXECUTE PROCEDURE check_if_time_flight_is_correct_update();
+
+CREATE OR REPLACE FUNCTION check_if_time_flight_is_correct_insert()
+  RETURNS trigger AS $$
+  DECLARE
+    minutes INTEGER;
+    interval_time VARCHAR(10);
+  BEGIN
+    minutes = calculate_flight_duration((SELECT distance
+                                           FROM flight_connections
+                                           WHERE id = NEW.flight_connection_id));
+    interval_time = minutes || ' min';
+
+    IF (NEW.scheduled_departure_time + interval_time::interval) != NEW.scheduled_arrival_time THEN
+      RAISE WARNING 'Flight time is wrong';
+      RETURN NULL;
+    end if;
+
+  RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER check_flight_time_insert
+  BEFORE INSERT ON flights
+  FOR EACH ROW
+  EXECUTE PROCEDURE check_if_time_flight_is_correct_insert();
