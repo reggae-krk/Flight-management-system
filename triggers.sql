4295feb78bf4d081aa2f399907969c4784824978
reggae-krk <monika.gorecka.agh@gmail.com>
2018-10-03 22:55:51 +0200

    Create trigger for base price

--- triggers.sql
+++ triggers.sql
@@ -18,6 +18,30 @@ $$
 LANGUAGE plpgsql;
 
 CREATE TRIGGER  check_distance
-  BEFORE INSERT ON flight_connections
+  BEFORE INSERT OR UPDATE ON flight_connections
   FOR EACH ROW
   EXECUTE PROCEDURE check_if_airplane_max_distance_is_bigger_than_connection_distance();
+
+CREATE OR REPLACE FUNCTION check_if_base_price_is_bigger_than_0()
+  RETURNS trigger AS $$
+  BEGIN
+    IF 0 >= NEW.base_price THEN
+      RAISE WARNING 'Base price cannot be lower than 0!';
+      RETURN NULL;
+    end if;
+
+  RETURN NEW;
+END;
+$$
+LANGUAGE plpgsql;
+
+CREATE TRIGGER check_base_price
+  BEFORE INSERT OR UPDATE ON flights
+  FOR EACH ROW
+  EXECUTE PROCEDURE check_if_base_price_is_bigger_than_0();
+
+-- check trigger
+-- INSERT INTO flights(SELECT generate_flight_id(), 22, to_timestamp('2018-10-01 10:00:00', 'YYYY-MM-DD HH24:MI:SS'), to_timestamp('2018-10-01 21:30:00', 'YYYY-MM-DD HH24:MI:SS'), null, null, 0::decimal);
+
+
+
