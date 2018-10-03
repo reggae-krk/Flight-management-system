d93cc563a32cca7b6d922cef65e52645cc249d50
reggae-krk <monika.gorecka.agh@gmail.com>
2018-10-03 10:04:49 +0200

    Add create_airports sql file

---
+++ create_airports.sql
@@ -0,0 +1,52 @@
+INSERT INTO airports (airport_code, airport_name, city_code) VALUES ('KRK', 'Krakow Balice', 'KRK'),
+                                                                    ('WAW', 'Lotnisko Chopina', 'WAW'),
+                                                                    ('WMI', 'Warszawa Modlin', 'WAW'),
+                                                                    ('KTW', 'Katowice-Pyrzowice', 'KTW'),
+                                                                    ('WRO', 'Wroclaw-Starachowice', 'WRO'),
+                                                                    ('POZ', 'Poznan-Lawica', 'POZ'),
+                                                                    ('GDN', 'Gdansk-Rebiechowo', 'GDN');
+
+INSERT INTO airports (airport_code, airport_name, city_code) VALUES ('EZE', 'Buenos Aires-Ezeiza', 'BUA');
+
+INSERT INTO airports (airport_code, airport_name, city_code) VALUES ('SYD', 'Sydney Airport', 'SYD'),
+                                                                    ('MEL', 'Melbourne Airport', 'MEL');
+
+INSERT INTO airports (airport_code, airport_name, city_code) VALUES ('VIE', 'Vienna-Schwechat', 'VIE'),
+                                                                    ('INN', 'Innsbruck-Kranebitten', 'INN');
+
+INSERT INTO airports (airport_code, airport_name, city_code) VALUES ('CRL', 'Brussels-Charleroi', 'BRU');
+
+INSERT INTO airports (airport_code, airport_name, city_code) VALUES ('MSQ', 'Minsk-2', 'MSQ');
+
+INSERT INTO airports (airport_code, airport_name, city_code) VALUES ('GIG', 'Rio de Janeiro-Galeao', 'RDJ');
+
+INSERT INTO airports (airport_code, airport_name, city_code) VALUES ('ZAG', 'Zagreb Airport', 'ZAG');
+
+INSERT INTO airports (airport_code, airport_name, city_code) VALUES ('LCA', 'Larnaca Airport', 'LCA');
+
+INSERT INTO airports (airport_code, airport_name, city_code) VALUES ('PRG', 'Vaclav Havel Airport Prague', 'PRG');
+
+INSERT INTO airports (airport_code, airport_name, city_code) VALUES ('CPH', 'Copenhagen-Kastrup', 'CPH');
+
+INSERT INTO airports (airport_code, airport_name, city_code) VALUES ('CDG', 'Roissy-Charles de Gaulle', 'PAR'),
+                                                                    ('ORY', 'Orly', 'PAR');
+
+INSERT INTO airports (airport_code, airport_name, city_code) VALUES ('MAD', 'Madrid-Barajas', 'MAD'),
+                                                                    ('BCN', 'Barcelona–El Prat', 'BCN');
+
+INSERT INTO airports (airport_code, airport_name, city_code) VALUES ('AMS', 'Amsterdam-Schiphol', 'AMS');
+
+INSERT INTO airports (airport_code, airport_name, city_code) VALUES ('YYC', 'Calgary International Airport', 'CAL');
+
+INSERT INTO airports (airport_code, airport_name, city_code) VALUES ('DOH', 'Hamad International Airport', 'DOH');
+
+INSERT INTO airports (airport_code, airport_name, city_code) VALUES ('OSL', 'Oslo-Gardermoen', 'OSL');
+
+INSERT INTO airports (airport_code, airport_name, city_code) VALUES ('DME', 'Moscow-Domodedovo', 'MOW'),
+                                                                    ('SVO', 'Moscow-Sheremetyevo', 'MOW');
+
+INSERT INTO airports (airport_code, airport_name, city_code) VALUES ('FCO', 'Leonardo da Vinci–Fiumicino Airport', 'ROM');
+
+INSERT INTO airports (airport_code, airport_name, city_code) VALUES ('STN', 'London-Stansted', 'LON'),
+                                                                    ('LHR', 'London-Heathrow', 'LON');
+
