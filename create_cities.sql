f5243cf1e0264bc83f5df03e321266401cec1a83
reggae-krk <monika.gorecka.agh@gmail.com>
2018-10-03 10:05:04 +0200

    Add create_cities sql file

---
+++ create_cities.sql
@@ -0,0 +1,51 @@
+INSERT INTO cities (city_code, city_name, country_code) VALUES ('KRK', 'Krakow', 'POL'),
+                                                               ('WAW', 'Warsaw', 'POL'),
+                                                               ('KTW', 'Katowice', 'POL'),
+                                                               ('WRO', 'Wroclaw', 'POL'),
+                                                               ('POZ', 'Poznan', 'POL'),
+                                                               ('GDN', 'Gdansk', 'POL');
+
+INSERT INTO cities (city_code, city_name, country_code) VALUES ('BUA', 'Buenos Aires', 'ARG');
+
+INSERT INTO cities (city_code, city_name, country_code) VALUES ('SYD', 'Sydney', 'AUS'),
+                                                               ('MEL', 'Melbourne', 'AUS');
+
+INSERT INTO cities (city_code, city_name, country_code) VALUES ('VIE', 'Vienna', 'AUT'),
+                                                               ('INN', 'Innsbruck', 'AUT');
+
+INSERT INTO cities (city_code, city_name, country_code) VALUES ('BRU', 'Brussels', 'BEL');
+
+INSERT INTO cities (city_code, city_name, country_code) VALUES ('MSQ', 'Minsk', 'BLR');
+
+INSERT INTO cities (city_code, city_name, country_code) VALUES ('RDJ', 'Rio de Janeiro', 'BRA');
+
+INSERT INTO cities (city_code, city_name, country_code) VALUES ('ZAG', 'Zagreb', 'HRV');
+
+INSERT INTO cities (city_code, city_name, country_code) VALUES ('LCA', 'Larnaca', 'CPR');
+
+INSERT INTO cities (city_code, city_name, country_code) VALUES ('PRG', 'Prague', 'CZE');
+
+INSERT INTO cities (city_code, city_name, country_code) VALUES ('CPH', 'Copenhagen', 'DNK');
+
+INSERT INTO cities (city_code, city_name, country_code) VALUES ('PAR', 'Paris', 'FRA');
+
+INSERT INTO cities (city_code, city_name, country_code) VALUES ('MAD', 'Madrid', 'ESP'),
+                                                               ('BCN', 'Barcelona', 'ESP');
+
+INSERT INTO cities (city_code, city_name, country_code) VALUES ('AMS', 'Amsterdam', 'NLD');
+
+INSERT INTO cities (city_code, city_name, country_code) VALUES ('CAL', 'Calgary', 'CAN');
+
+INSERT INTO cities (city_code, city_name, country_code) VALUES ('DOH', 'Doha', 'QAT');
+
+INSERT INTO cities (city_code, city_name, country_code) VALUES ('OSL', 'Oslo', 'QAT');
+
+INSERT INTO cities (city_code, city_name, country_code) VALUES ('MOW', 'Moscow', 'RUS');
+
+INSERT INTO cities (city_code, city_name, country_code) VALUES ('ROM', 'Rome', 'ITA');
+
+INSERT INTO cities (city_code, city_name, country_code) VALUES ('LON', 'London', 'GBR');
+
+
+
+
