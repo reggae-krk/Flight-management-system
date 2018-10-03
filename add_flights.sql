2dca21c55ceb7e81e49075b18ea3b103094d6bcd
edobkowski <edobkowski@gmail.com>
2018-10-03 08:47:56 +0200

    Implement function inserting generated flights into a table

---
+++ add_flights.sql
@@ -0,0 +1,33 @@
+-- add flight with return flight for each airplane in the fleet
+CREATE OR REPLACE FUNCTION add_flights(start_date TIMESTAMP)
+  RETURNS VOID AS $$
+DECLARE
+  start_date_return TIMESTAMP := start_date + '5 hours';
+  departure_date TIMESTAMP;
+  departure_date_return TIMESTAMP;
+BEGIN
+
+  FOR i IN 0..7 LOOP
+      departure_date := start_date + ((i || 'days')::INTERVAL);
+      departure_date_return := start_date_return + ((i || 'days')::INTERVAL);
+      RAISE INFO 'inserting for date: %', departure_date;
+
+      INSERT INTO flights (id, flight_connection_id, scheduled_departure_time, scheduled_arrival_time, base_price)
+          (SELECT generate_flight_id(),
+                  id,
+                  CASE
+                    WHEN id%2 != 0 THEN departure_date
+                    ELSE departure_date_return
+                  END,
+                  CASE
+                    WHEN id%2 != 0 THEN (departure_date + ((calculate_flight_duration(distance) || 'minutes')::INTERVAL))
+                    ELSE (departure_date_return + ((calculate_flight_duration(distance) || 'minutes')::INTERVAL))
+                  END,
+                  calculate_flight_cost(distance, departure_date)
+          FROM flight_connections);
+  END LOOP;
+END;
+$$
+  LANGUAGE plpgsql;
+
+SELECT add_flights('2018-10-02 10:00:00'::TIMESTAMP);
