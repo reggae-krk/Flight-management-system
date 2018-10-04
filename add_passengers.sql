a8122d6bf6eb07039eb36aa42418d3c4c6a0a76d
reggae-krk <monika.gorecka.agh@gmail.com>
2018-10-04 09:55:59 +0200

    Add country column

--- add_passengers.sql
+++ add_passengers.sql
@@ -2,7 +2,8 @@ CREATE OR REPLACE FUNCTION add_passengers(n INTEGER)
   RETURNS VOID AS $$
 BEGIN
   FOR i IN 1..n LOOP
-      INSERT INTO passengers (first_name, last_name) SELECT first_name, last_name FROM get_random_full_name();
+      INSERT INTO passengers (first_name, last_name, country) SELECT first_name, last_name, country FROM get_random_full_name_and_country();
+ get_random_full_name();
   END LOOP;
 END;
 $$
