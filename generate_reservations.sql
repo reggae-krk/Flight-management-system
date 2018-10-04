262f3904048dbea6418718ebaec1b3f66f4e5143
edobkowski <edobkowski@gmail.com>
2018-10-04 11:11:03 +0200

    Implement function for generating reservations

---
+++ generate_reservations.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE FUNCTION generate_reservations(n INTEGER)
+  RETURNS VOID AS $$
+BEGIN
+  WHILE (SELECT count(*) FROM reservations) < n LOOP
+    INSERT INTO reservations (flight_id, passenger_id, seat_number, is_child)
+        SELECT f.id AS flight_id, p.id AS passenger_id, generate_seat(m.max_seats, f.id) AS assigned_seat, FALSE AS is_child
+          FROM flights f
+          CROSS JOIN passengers p
+          LEFT JOIN models m ON m.id = (SELECT model_id FROM airplanes a
+                                        WHERE a.id = (SELECT airplane_id FROM flight_connections
+                                                     WHERE id = f.flight_connection_id))
+          ORDER BY random()
+          LIMIT 1;
+  END LOOP;
+END;
+$$
+  LANGUAGE plpgsql;
