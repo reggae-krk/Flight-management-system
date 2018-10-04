b83ee50c4b73a5d88b5ef6d0b88e02206de852c1
szymslow12 <szym.slow12@gmail.com>
2018-10-04 10:55:46 +0200

    fix -> calculates income from month not from a day

--- calculate_income_per_month.sql
+++ calculate_income_per_month.sql
@@ -1,6 +1,7 @@
 CREATE OR REPLACE FUNCTION calculate_income_per_month(date DATE)
   RETURNS DECIMAL AS $$
 DECLARE
+  month INTEGER := DATE_PART('month', date);
   flights_record RECORD;
   reservation RECORD;
   flightIDs CHAR(6)[] := '{}';
@@ -8,21 +9,19 @@ DECLARE
 BEGIN
   FOR flights_record IN
     SELECT id, scheduled_departure_time FROM flights
-    WHERE scheduled_departure_time::DATE = date
+    WHERE DATE_PART('month', scheduled_departure_time::DATE) = month
   LOOP
-    flightIDs := array_append(flightIDs, flights_record.id);
+    flightIDs := ARRAY_APPEND(flightIDs, flights_record.id);
   END LOOP;
 
-  FOR i IN 1..array_length(flightIDs, 1) LOOP
+  FOR i IN 1..ARRAY_LENGTH(flightIDs, 1) LOOP
     FOR reservation IN
       SELECT id FROM reservations
       WHERE flight_id = flightIDs[i]
     LOOP
       income := income + calculate_ticket_cost(reservation.id);
-    END LOOP ;
+    END LOOP;
   END LOOP;
-
-  RAISE NOTICE USING MESSAGE = flightIDs;
   RETURN income;
 END;
 $$
